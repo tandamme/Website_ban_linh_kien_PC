f4e566e15bcdaea789fba33dfb0a448a088b4a6c
Linh2404XD <21130092@st.hcmuaf.edu.vn>
2025-12-08 12:57:11 +0700

    edit layout

--- Do-an-lap-trinh-c#/obj/Debug/net8.0/Do-an-lap-trinh-c#.AssemblyInfo.cs
+++ Do-an-lap-trinh-c#/obj/Debug/net8.0/Do-an-lap-trinh-c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827a5655843318ff408cb4e83ca74df5c57fe71c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0aa1f089cc7f0b21223b28076c2657880c4f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
