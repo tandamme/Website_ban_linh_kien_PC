29bfee1f09f88823651da65b31aafeae9671f5f0
Linh2404XD <21130092@st.hcmuaf.edu.vn>
2026-01-14 13:08:53 +0700

    validation

--- Do-an-lap-trinh-c#/ViewModels/RegisterVM.cs
+++ Do-an-lap-trinh-c#/ViewModels/RegisterVM.cs
@@ -4,20 +4,22 @@ namespace Do_an_lap_trinh_c_.ViewModels
 {
     public class RegisterVM
     {
-        [Required]
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
         public string userName { get; set; }
 
-        [Required]
-        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+
         public string email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Mật khẩu tối thiểu 6 ký tự")]
         public string passWord { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vui lòng nhập lại mật khẩu")]
         [DataType(DataType.Password)]
-        [Compare("passWord", ErrorMessage = "Password không khớp")]
+        [Compare("passWord", ErrorMessage = "Mật khẩu nhập lại không khớp")]
         public string rePassWord { get; set; }
     }
 
--- Do-an-lap-trinh-c#/obj/Debug/net8.0/Do-an-lap-trinh-c#.AssemblyInfo.cs
+++ Do-an-lap-trinh-c#/obj/Debug/net8.0/Do-an-lap-trinh-c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6d86db148b956e98b629f6ef6aacedacb8cdf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bab48101ade94278278d141fa931c3785fb18b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do-an-lap-trinh-c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
