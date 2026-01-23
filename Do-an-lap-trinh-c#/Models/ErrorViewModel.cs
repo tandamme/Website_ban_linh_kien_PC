efac10318aef3e4fe992d695b2aa14f0a76fd4fd
Linh2404XD <147931651+Linh2404XD@users.noreply.github.com>
2026-01-23 14:43:35 +0700

    Delete Do-an-lap-trinh-c#/Models directory

--- Do-an-lap-trinh-c#/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Do_an_lap_trinh_c_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
