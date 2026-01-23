e8fe38e299dc1bdf1fb6f674e697595a418751d8
Linh2404XD <147931651+Linh2404XD@users.noreply.github.com>
2026-01-23 22:37:10 +0700

    phan quyen

---
+++ Do-an-lap-trinh-c#/Do-an-lap-trinh-c#/Extensions/AuthorizeHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Do_an_lap_trinh_c_.Extensions
+{
+    public static class AuthorizeHelper
+    {
+        public static bool IsLoggedIn(this HttpContext context)
+        {
+            return context.Session.GetInt32("UserId") != null;
+        }
+
+        public static bool IsAdmin(this HttpContext context)
+        {
+            return context.Session.GetInt32("Role") == 0;
+        }
+
+        public static bool IsUser(this HttpContext context)
+        {
+            return context.Session.GetInt32("Role") == 1;
+        }
+
+
+
+
+    }
+
+
+}
